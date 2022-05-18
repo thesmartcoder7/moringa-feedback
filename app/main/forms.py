db99ade6a9cb9515344701977d51545a361e4893
samuel martins <samuel.martins4.sm@gmail.com>
2022-05-19 02:10:42 +0300

    feat: create the signup, login, and logout route functions

---
+++ app/main/forms.py
@@ -0,0 +1,47 @@
+from flask import render_template, redirect, url_for, session, request
+from app import db
+from . import main
+from app.models import User
+from werkzeug.security import generate_password_hash, check_password_hash
+
+
+# user signup route function
+@main.route('/signup', methods=['POST', 'GET'])
+def signup():
+    if request.method == 'POST':
+        email = request.form['s-email']
+        name = request.form['s-name']
+        password = request.form['s-password']
+        if email and name and password:
+            new_user = User(name, email, generate_password_hash(password))
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = name
+            session['password'] = password
+            return redirect(url_for('main.dashboard', user=new_user), code=307)
+        else:
+            # return a flash message for an unsuccessful signup
+            return redirect(url_for('main.home'))
+    else:
+        return redirect(url_for('main.home'))
+
+
+# user login route function
+@main.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        email = request.form['l-email']
+        password = request.form['l-password']
+        user = User.query.filter_by(email=email).first()
+        if user:
+            if check_password_hash(user.password, password):
+                session['username'] = user.name
+                session['password'] = user.password
+                return redirect(url_for('main.dashboard', username=user.name), code=307)
+            else:
+                # include a flash message here to indicate wrong password
+                return redirect(url_for('main.home'))
+        else:
+            return redirect(url_for('main.home'))
+    else:
+        return redirect(url_for('main.home'))
--- app/main/views.py
+++ app/main/views.py
@@ -1,17 +1,28 @@
-from flask import render_template
+
+from crypt import methods
+from flask import render_template, redirect, url_for, request, session
 from . import main
+from app.models import User
 
 
 @main.route('/')
 def home():
-    return render_template('index.html')
+    if session:
+        return redirect(url_for('main.dashboard', username=session['username']))
+    else:
+        return render_template('index.html')
 
 
-@main.route('/user')
-def dashboard():
-    return render_template('user.html')
+@main.route('/user/<username>', methods=['POST', 'GET'])
+def dashboard(username):
+    user = User.query.filter_by(name=username).first()
+    return render_template('user.html', user=user)
+    
 
-
+@main.route('/logout')
+def sign_out():
+    session.clear()
+    return redirect(url_for('main.home'))
 
 
 
