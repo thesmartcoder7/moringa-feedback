6bcc299d4e774b1b2e4d781160ff595dd0260b11
samuel martins <samuel.martins4.sm@gmail.com>
2022-06-27 00:01:43 +0300

    patch: tweak main application for better deployment

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
-from config import Config
+from ..config import Config
 
 
 db = SQLAlchemy()
@@ -16,9 +16,9 @@ def create_app():
 
     db.init_app(app)
     migrate.init_app(app, db)
-    from app import models
+    from . import models
 
-    from app.main import main
+    from .main import main
     app.register_blueprint(main)
 
     return app
